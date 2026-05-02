738bb9cb91f25ba628b17955de1fcb06dcbd4055
Delan18 <melanyeya@icloud.com>
2026-05-02 16:02:32 -0400

    add rerouting

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate, useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
 import Home from './pages/Home'
 import ProjectDetail from './pages/ProjectDetail'
 import ExperienceDetail from './pages/ExperienceDetail'
+import ScrollToTop from './components/ScrollToTop'
+
+function RedirectHandler() {
+  const navigate = useNavigate()
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search)
+    const redirect = params.get('redirect')
+    if (redirect) {
+      navigate(redirect, { replace: true })
+    }
+  }, [])
+  return null
+}
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
+      <RedirectHandler />
       <Routes>
         <Route path="/aero" element={<Home client="aero" />} />
         <Route path="/corporate" element={<Home client="corporate" />} />
